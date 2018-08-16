8d3a219423294c56b92885a885fc0521daf3a547
Ava Barron <mztriz@gmail.com>
2018-08-16 16:41:59 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,7 +31,8 @@ function removeElementFromBeginningOfArray(array){
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
-  return array.pop()
+  const array = array.pop()
+  return array
 }
 
 function removeElementFromEndOfArray(array){
